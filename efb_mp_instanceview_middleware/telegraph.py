d757dd524fd4eaa5def925d138af0616da10e91a
catbaron0 <catbaron@live.cn>
2020-08-26 01:49:33 +0900

    rm styles to avoid CONTENT_TOO_BIG error

--- efb_mp_instanceview_middleware/telegraph.py
+++ efb_mp_instanceview_middleware/telegraph.py
@@ -1,5 +1,7 @@
 import json
 import requests
+import urllib3
+import certifi
 from typing import List, Dict
 from bs4 import BeautifulSoup as bs
 from bs4 import Tag, NavigableString
@@ -15,16 +17,13 @@ class Telegraph:
         }
         self.publish_url = 'https://api.telegra.ph/createPage'
         self.token = token
-        # self.http = urllib3.PoolManager(
-        #     cert_reqs="CERT_REQUIRED",
-        #     ca_certs=certifi.where()
-        # )
+        # self.spider = urllib3.PoolManager(cert_reqs="CERT_REQUIRED", ca_certs=certifi.where())
+        self.spider = requests.Session()
         proxies = {
             'http': proxy,
             'https': proxy
         }
         self.publisher = requests.Session()
-        self.spider = requests.Session()
         if proxy:
             self.publisher.proxies = proxies
 
@@ -58,7 +57,6 @@ class Telegraph:
         }
         # response = self.http.request('POST', self.publish_url, fields=fields).data.decode('utf-8')
         # response = json.loads(response)
-        print("Publisher.proxy", self.publisher.proxies)
         response = self.publisher.post(self.publish_url, data=fields).json()
         return response['result']['url']
 
@@ -67,13 +65,17 @@ class Telegraph:
         if type(tag) == NavigableString:
             return str(tag)
         tag_name = tag.name
-        if tag_name == 'br':
+        if tag_name == 'br' or tag_name == 'script':
             return ''
         node['tag'] = tag_name if tag_name in self.tags else 'p'
         attrs = tag.attrs
         if 'display: none' in attrs.get('style', '') or \
             'visibility: hidden' in attrs.get('style', ''):
             attrs['hidden'] = 'True'
+        try:
+            del(attrs['style'])
+        except KeyError:
+            pass
         if tag_name == 'img':
             attrs['src'] = attrs.get('data-src', '')
         node['attrs'] = attrs
@@ -86,7 +88,8 @@ class Telegraph:
         '''
         Return author, title and content from a given url
         '''
-        print("Spider.proxy", self.splider.proxies)
+        # res = self.spider.request('get', url)
+        # soup = bs(res.data.decode('utf-8'), features="html.parser")
         res = self.spider.get(url)
         soup = bs(res.text, features="html.parser")
 
@@ -113,11 +116,7 @@ class Telegraph:
 
 
 if __name__ == '__main__':
-    ACCESS_TOKEN = '1acbef8d50b3ba8d0d0db262a413f545320d335fae6dc4cc4a860f7c6c26'
+    ACCESS_TOKEN = ''
     tg = Telegraph(ACCESS_TOKEN)
-    page = tg.process_url(
-        # 'https://mp.weixin.qq.com/s?__biz=MjM5NzE1NTMyNg==&mid=2650934190&idx=5&sn=72c8016694181b337a48fba178a26355&chksm=bd28f33f8a5f7a29bd27494dbf06859aad5ce0a663eef46fa2e5443a3774b7fbec82a91522ed&scene=0&xtrack=1#rd'
-        # 'https://mp.weixin.qq.com/s?__biz=MzUwOTg3NjIwNg==&mid=2247525393&idx=5&sn=77089269b97ed4a96a835a3ca0747b30&chksm=f909a690ce7e2f86a320136b7f6b7b6dc29d6c89585c6b5610ab8709687e7b238078a51e557f&scene=0&xtrack=1#rd'
-        'http://mp.weixin.qq.com/s?__biz=MjM5NzE1NTMyNg==&mid=2650934220&idx=1&sn=8f02f1efa9963fa787484e0ca86101f5&chksm=bd28f35d8a5f7a4be54547d04fc1df0069970685b3d33f69c4433c5f35e370f138fac62864fa&scene=0&xtrack=1#rd'
-    )
+    page = tg.process_url('')
     print(tg.publish(page['title'], page['author'], page['content']))
